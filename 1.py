8928e47649764cf66682cf38752133d9b74c0ca5
NMSYASWANTH <56400581+NMSYASWANTH@users.noreply.github.com>
2021-10-02 23:12:00 +0530

    Create 1.py

---
+++ 1.py
@@ -0,0 +1,19 @@
+t=int(input())
+for _ in range(t):
+    n=int(input())
+    k=[int(x) for x in input().split()[0:n*n]]
+    mat=[]
+    s=0
+    for i in range(n):
+        x=[]
+        for j in range(n):
+            x.append(k[s])
+            s+=1
+        mat.append(x)
+        w=[]
+    for j in range(n):
+        for i in range(n):
+            q=mat[i][n-j-1]
+            w.append(q)
+    print(*w)
+        
